ca0801c7db85ce0e3321c4bba3aadc8e273d8228
mkrivoseina <78380499+mkrivoseina@users.noreply.github.com>
2021-03-25 21:29:52 +0200

    Initial commit

---
+++ js/main2.js
@@ -0,0 +1,7 @@
+function changeColor() {
+    const colorForH2 = document.getElementById('h2-color-changed');
+    colorForH2.style.color = 'red'
+    colorForH2.style.fontSize = '50px'
+    colorForH2.style.fontFamily = 'verdana'
+
+}
